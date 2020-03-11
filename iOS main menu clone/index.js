3d245cf3e81922defaf8cc58da54e988065f2cbe
INoobSaibot <INoobSaibot@users.noreply.github.com>
2020-03-10 21:09:22 -0400

    added touch controls

--- iOS main menu clone/index.js
+++ iOS main menu clone/index.js
@@ -1,7 +1,7 @@
 const Index = {
-    init: function() {
+    init: function () {
         // dev testing start at #0 search screen
-        // this.left();
+
         // end dev only code
 
         $(".home-button").click(this.home);
@@ -13,17 +13,22 @@ const Index = {
 
             if (key === 'ArrowRight') {
                 this.right();
-            } else
-                if (key === 'ArrowLeft') {
-                    this.left();
-                } else if (key === 'ArrowUp') {
-                    this.home();
-                }
+            } else if (key === 'ArrowLeft') {
+                this.left();
+            } else if (key === 'ArrowUp') {
+                this.home();
+            }
         }.bind(this));
 
+        this.touchmoveRegister(this.right, this.left, this.home);
+
+        document.getElementById("container").addEventListener("transitionend", myFunction);
+        function myFunction(e) {
+            console.log(e);
+        }
     },
 
-    home: function() {
+    home: function () {
         let grid_0_in_view = $('#view-0').hasClass('focused-position');
         let grid_1_in_view = $('#view-1').hasClass('focused-position');
         let grid_2_in_view = $('#view-2').hasClass('focused-position');
@@ -33,35 +38,35 @@ const Index = {
             // do nothing, already at home
         }
 
-        if(grid_0_in_view) {
-            $( '#view-0').toggleClass( 'left-transition');
-            $( '#view-0').toggleClass( 'focused-position');
-            $( '#view-dot-0').toggleClass('active');
+        if (grid_0_in_view) {
+            $('#view-0').toggleClass('left-transition');
+            $('#view-0').toggleClass('focused-position');
+            $('#view-dot-0').toggleClass('active');
 
-            $( '#view-1').toggleClass( 'focused-position');
-            $( '#view-dot-1').toggleClass('active');
+            $('#view-1').toggleClass('focused-position');
+            $('#view-dot-1').toggleClass('active');
 
             $('#bottom').toggleClass('focused-position');
         }
 
         if (grid_2_in_view) {
-            $( '#view-2').toggleClass( 'focused-position');
-            $( '#view-dot-2').toggleClass('active');
+            $('#view-2').toggleClass('focused-position');
+            $('#view-dot-2').toggleClass('active');
 
-            $( '#view-1').toggleClass( 'left-transition');
-            $( '#view-1').toggleClass( 'focused-position');
-            $( '#view-dot-1').toggleClass('active');
+            $('#view-1').toggleClass('left-transition');
+            $('#view-1').toggleClass('focused-position');
+            $('#view-dot-1').toggleClass('active');
         }
 
         if (grid_3_in_view) {
-            $( '#view-3').toggleClass( 'focused-position');
-            $( '#view-dot-3').toggleClass('active');
+            $('#view-3').toggleClass('focused-position');
+            $('#view-dot-3').toggleClass('active');
 
-            $( '#view-2').toggleClass( 'left-transition');
-            $( '#view-1').toggleClass( 'left-transition');
+            $('#view-2').toggleClass('left-transition');
+            $('#view-1').toggleClass('left-transition');
 
-            $( '#view-1').toggleClass( 'focused-position');
-            $( '#view-dot-1').toggleClass('active');
+            $('#view-1').toggleClass('focused-position');
+            $('#view-dot-1').toggleClass('active');
         }
 
     },
@@ -73,31 +78,31 @@ const Index = {
         let grid_3_in_view = $('#view-3').hasClass('focused-position');
 
         if (grid_0_in_view) {
-            $( '#view-0').toggleClass( 'left-transition');
-            $( '#view-0').toggleClass( 'focused-position');
-            $( '#view-dot-0').toggleClass('active');
+            $('#view-0').toggleClass('left-transition');
+            $('#view-0').toggleClass('focused-position');
+            $('#view-dot-0').toggleClass('active');
 
-            $( '#view-1').toggleClass( 'focused-position');
-            $( '#view-dot-1').toggleClass('active');
+            $('#view-1').toggleClass('focused-position');
+            $('#view-dot-1').toggleClass('active');
 
             $('#bottom').toggleClass('focused-position');
         }
 
         if (grid_1_in_view) {
-            $( '#view-1').toggleClass( 'left-transition');
-            $( '#view-1').toggleClass( 'focused-position');
-            $( '#view-dot-1').toggleClass('active');
+            $('#view-1').toggleClass('left-transition');
+            $('#view-1').toggleClass('focused-position');
+            $('#view-dot-1').toggleClass('active');
 
-            $( '#view-2').toggleClass( 'focused-position');
-            $( '#view-dot-2').toggleClass('active');
+            $('#view-2').toggleClass('focused-position');
+            $('#view-dot-2').toggleClass('active');
         }
         if (grid_2_in_view) {
-            $( '#view-2').toggleClass( 'left-transition');
-            $( '#view-2').toggleClass( 'focused-position');
-            $( '#view-dot-2').toggleClass('active');
+            $('#view-2').toggleClass('left-transition');
+            $('#view-2').toggleClass('focused-position');
+            $('#view-dot-2').toggleClass('active');
 
-            $( '#view-3').toggleClass( 'focused-position');
-            $( '#view-dot-3').toggleClass('active');
+            $('#view-3').toggleClass('focused-position');
+            $('#view-dot-3').toggleClass('active');
 
         }
     },
@@ -108,32 +113,80 @@ const Index = {
         let grid_3_in_view = $('#view-3').hasClass('focused-position');
 
         if (grid_1_in_view) {
-            $( '#view-1').toggleClass( 'focused-position');
-            $( '#view-dot-1').toggleClass('active');
+            $('#view-1').toggleClass('focused-position');
+            $('#view-dot-1').toggleClass('active');
 
-            $( '#view-0').toggleClass( 'left-transition');
-            $( '#view-0').toggleClass( 'focused-position');
-            $( '#view-dot-0').toggleClass('active');
+            $('#view-0').toggleClass('left-transition');
+            $('#view-0').toggleClass('focused-position');
+            $('#view-dot-0').toggleClass('active');
 
             $('#bottom').toggleClass('focused-position');
-        }
+        } else if (grid_2_in_view) {
+            $('#view-2').toggleClass('focused-position');
+            $('#view-dot-2').toggleClass('active');
 
-        else if (grid_2_in_view) {
-            $( '#view-2').toggleClass( 'focused-position');
-            $( '#view-dot-2').toggleClass('active');
-
-            $( '#view-1').toggleClass( 'left-transition');
-            $( '#view-1').toggleClass( 'focused-position');
-            $( '#view-dot-1').toggleClass('active');
+            $('#view-1').toggleClass('left-transition');
+            $('#view-1').toggleClass('focused-position');
+            $('#view-dot-1').toggleClass('active');
         }
 
         if (grid_3_in_view) {
-            $( '#view-3').toggleClass( 'focused-position');
-            $( '#view-dot-3').toggleClass('active');
+            $('#view-3').toggleClass('focused-position');
+            $('#view-dot-3').toggleClass('active');
 
-            $( '#view-2').toggleClass( 'left-transition');
-            $( '#view-2').toggleClass( 'focused-position');
-            $( '#view-dot-2').toggleClass('active');
+            $('#view-2').toggleClass('left-transition');
+            $('#view-2').toggleClass('focused-position');
+            $('#view-dot-2').toggleClass('active');
         }
-    }
+    },
+
+    touchmoveRegister: function (right, left, home) {
+        let touchsurface = document.getElementById('container'),
+            startX,
+            startY,
+            dist,
+            threshold = 25, //required min distance traveled to be considered swipe
+            allowedTime = 2000, // maximum time allowed to travel that distance
+            elapsedTime,
+            startTime;
+
+        const handleSwipe = (isRightswipe, isLeftSwipe) => {
+            if (isRightswipe) {
+                left();
+            } else if (isLeftSwipe) {
+                right();
+            } else {
+                home();
+            }
+        }
+
+        touchsurface.addEventListener('touchstart', function (e) {
+            const touchobj = e.changedTouches[0];
+            dist = 0;
+            startX = touchobj.pageX;
+            startY = touchobj.pageY;
+            startTime = new Date().getTime(); // record time when finger first makes contact with surface
+            e.preventDefault()
+        }, false);
+
+        touchsurface.addEventListener('touchmove', function (e) {
+            e.preventDefault() // prevent scrolling when inside DIV
+        }, false);
+
+        touchsurface.addEventListener('touchend', function (e) {
+            let touchobj = e.changedTouches[0];
+            dist = touchobj.pageX - startX; // get total dist traveled by finger while in contact with surface
+
+            elapsedTime = new Date().getTime() - startTime; // get time elapsed
+            // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+            let isRightSwipe = (elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
+            let isLeftSwipe = (elapsedTime <= allowedTime && (-dist) >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
+
+            handleSwipe(isRightSwipe, isLeftSwipe);
+            e.preventDefault()
+        }, false)
+
+
+
+    }.bind(this)
 }
